1d7b15ee3d9c56bbe0049e2f6f04dbbae9a935e3
= <murilobdo@hotmail.com>
2024-09-21 14:25:00 -0300

    Criando a branch de desenvolvimento

--- Fighter.API/Program.cs
+++ Fighter.API/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World From Github Actions!");
+app.MapGet("/", () => "Hello World From Github Actions From Develop!");
 
 app.Run();
